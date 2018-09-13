b281078b4641771008f55caf72efa6839819a561
姚瀚晨 <Stephen_Yao0@126.com>
2018-09-13 22:40:41 +0800

    Update C++ Class25_标准库_bitset类型.cpp

--- C++ Class25_标准库_bitset类型.cpp
+++ C++ Class25_标准库_bitset类型.cpp
@@ -1,1 +1,18 @@
-Class25_标准库_bitset类型
+//20180912
+//Class25_标准库_bitset类型
+//Demo56
+#include<iostream>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	bitset<32>a;//a的大小是32位二进制，全部是0
+	cout << a << endl;
+
+	bitset<16>b(0xffff);
+	cout << b << endl;
+	return 0;
+}
