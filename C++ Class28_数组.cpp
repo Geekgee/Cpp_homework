38987a6b10de7274d48531174e5b04ae44696206
姚瀚晨 <Stephen_Yao0@126.com>
2018-09-21 23:57:49 +0800

    Update C++ Class28_数组.cpp

--- C++ Class28_数组.cpp
+++ C++ Class28_数组.cpp
@@ -1,12 +1,29 @@
-//20180920
+//20180921
 //Class28_数组
-//Demo62 
+//Demo62 数组的初始化
 #include<iostream>
-#include<bitset>
+#include<string>
 using namespace std;
+
+unsigned get_size()
+{
+	int a = 100;
+	int b = 200;
+	return 0;
+}
 int main()
 {
+	int a[100];
+	const unsigned buf_size = 512, max_files = 20;
+	int staff_size = 27;
+	const unsigned sz = get_size();
+	char input_buffer[buf_size];
+	string fileTable[max_files + 1];
 
+	//double salaries[staff_size];
+	//int test_scores[get_size()];
+	//int vals[sz];编译的时候没有值，不行
 
+	cout << "OK" << endl;
 	return 0;
 }
